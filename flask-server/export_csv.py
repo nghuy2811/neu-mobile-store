0b7e7f25eb7cfda2eca89fdef59c94546f54159a
Huy Nguyen <nghuy2811@gmail.com>
2022-03-28 23:26:17 +0700

    add database file, convert db file to csv file script

---
+++ flask-server/export_csv.py
@@ -0,0 +1,36 @@
+import sqlite3 as sql
+
+
+def fetch_table_data(table_name):
+    cnx = sql.connect('smartphone.db')
+
+    cursor = cnx.cursor()
+    cursor.execute('select * from ' + table_name)
+
+    header = [row[0] for row in cursor.description]
+
+    rows = cursor.fetchall()
+
+    # Closing connection
+    cnx.close()
+
+    return header, rows
+
+
+def export(table_name):
+    header, rows = fetch_table_data(table_name)
+
+    # Create csv file
+    f = open(table_name + '.csv', 'w', encoding="utf-8")
+
+    # Write header
+    f.write(','.join(header) + '\n')
+
+    for row in rows:
+        f.write(','.join(str(r) for r in row) + '\n')
+
+    f.close()
+    print(str(len(rows)) + ' rows written successfully to ' + f.name)
+
+
+export("smartphones")
